669d9a861e0af8711c9979935c4f662d2e665e5f
Gabriel Alexandre Nascimento <gabriel.nascimento@voxsolucoes.com.br>
2025-11-14 14:32:35 -0300

    Criação model produto

---
+++ Models/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase5.Classes
+{
+    internal class Produto
+    {
+        private string idProduto;
+        private string nomeProduto;
+        private int quantidadeEstoque;
+        private string categoria;
+        private string descricao;
+
+        public string NomeProduto { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public string Categoria { get; set; }
+        public string Descricao { get; set; }
+    }
+}
