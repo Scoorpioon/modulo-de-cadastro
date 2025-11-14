7aa9fa15f1044504a802e6aa28120ba722afd452
Gabriel Alexandre Nascimento <gabriel.nascimento@voxsolucoes.com.br>
2025-11-14 15:10:40 -0300

    Criação do service de produto

---
+++ Services/FuncoesProduto.cs
@@ -0,0 +1,27 @@
+﻿using Fase5.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase5.Services
+{
+    internal class FuncoesProduto
+    {
+        private int idAtual = 0;
+
+        public Produto CriarProduto(string nome, int quantidadeEstoque, string categoria, string descricao)
+        {
+            Produto novoProduto = new Produto(
+                idAtual++,
+                nome,
+                quantidadeEstoque,
+                categoria,
+                descricao
+            );
+
+            return novoProduto;
+        }
+    }
+}
