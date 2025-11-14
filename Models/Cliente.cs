6aaa7db7fa7eb8386d1ee0d9f4c8a17f6a24e6da
Gabriel Alexandre Nascimento <gabriel.nascimento@voxsolucoes.com.br>
2025-11-14 14:29:58 -0300

    Criação do model de cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase5.Classes
+{
+    internal class Cliente
+    {
+        private int idCliente;
+        private string nome;
+        private string endereco;
+        private string cpf;
+
+        public Cliente(int idCliente, string nome, string endereco,  string cpf)
+        {
+            this.idCliente = idCliente;
+            this.nome = nome;
+            this.endereco = endereco;
+            this.cpf = cpf;
+        }
+
+        public string Nome {  get { return nome; } }
+        public string Endereco { get { return endereco; } }
+        public string Cpf { get { return cpf; } }
+    }
+}
